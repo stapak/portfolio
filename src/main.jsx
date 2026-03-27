f961f8bb18fc038c47ef6ed5bfdba79601c57bd2
stapak <125467248+stapak@users.noreply.github.com>
2026-03-28 04:53:53 +0530

    Version 1.1 sabotaged developer tools

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Disable right click globally
+document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+// Disable some DevTools shortcuts
+document.onkeydown = function (e) {
+  if (e.keyCode === 123) return false; // F12
+  if (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) return false;
+  if (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) return false;
+};
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
